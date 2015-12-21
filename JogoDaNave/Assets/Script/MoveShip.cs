ef54a482df4b69760b62d477cfc3214f9ab9634e
Henrique Assunção dos Santos <hnrq.ss@outlook.com>
2015-12-21 11:47:36 -0800

    Vitual-Joystick1
    for android

---
+++ JogoDaNave/Assets/Script/MoveShip.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShip : MonoBehaviour {
+
+	// Use this for initialization
+	public float moveSpeed = 5.0f;
+	public float drag = 0.5f;
+	public float terminalRotationSpeed = 25.0f;
+	public Vector3 moveVector{set;get;}
+	public VirtualJoystick joystick;
+
+	public Rigidbody thisRigidbody;
+
+	void Start () {
+		//thisRigidbody = gameObject.AddComponent<Rigidbody>();
+		thisRigidbody.maxAngularVelocity = terminalRotationSpeed;
+		thisRigidbody.drag = drag;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		moveVector = PoolInput();
+		move();
+	}
+
+	private void move()
+	{
+		thisRigidbody.AddForce((moveVector*moveSpeed));
+	}
+	private Vector3 PoolInput()
+	{
+		Vector3 dir = Vector3.zero;
+		//dir.x  = Input.GetAxis("Horizontal");
+		//dir.z  = Input.GetAxis("Vertical");
+		dir.x = joystick.Horizontal();
+		dir.y = joystick.Vertical();
+		//transform.rotation = Quaternion.AngleAxis(30, dir);
+		if(dir.magnitude > 1)
+			dir.Normalize();
+		return dir;
+
+
+	}
+}
---
+++ JogoDaNave/Assets/Script/VirtualJoystick.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+public class VirtualJoystick : MonoBehaviour, IDragHandler , IPointerUpHandler , IPointerDownHandler {
+
+	// Use this for initialization
+	private Image bgAnalogico;
+	private Image analogico;
+	private Vector3 inputVector;
+
+	void Start () {
+		bgAnalogico = GetComponent<Image>();
+		analogico = transform.GetChild(0).GetComponent<Image>();
+	}
+
+	public virtual void OnDrag (PointerEventData ped)
+	{
+		Vector2 pos ;
+		if(RectTransformUtility.ScreenPointToLocalPointInRectangle(bgAnalogico.rectTransform
+		                                                           ,ped.position
+		                                                           ,ped.pressEventCamera
+		                                                           ,out pos ))
+		{
+			//Debug.Log("E ai?");
+			pos.x = (pos.x / bgAnalogico.rectTransform.sizeDelta.x);
+			pos.y = (pos.y / bgAnalogico.rectTransform.sizeDelta.y);
+			inputVector = new Vector3 ((pos.x*2+1),0,(pos.y*2-1));
+			inputVector =  (inputVector.magnitude > 1.0f)? inputVector.normalized : inputVector; 
+			//Debug.Log(inputVector);
+			// mover o analogico
+			analogico.rectTransform.anchoredPosition = new Vector3 ( inputVector.x*(bgAnalogico.rectTransform.sizeDelta.x/2.5f)
+			                                                        ,inputVector.z*(bgAnalogico.rectTransform.sizeDelta.y/2.5f));
+		}
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped)
+	{ 
+		OnDrag(ped);
+	}
+	
+	public virtual void OnPointerUp(PointerEventData ped)
+	{
+		inputVector = Vector3.zero;
+		analogico.rectTransform.anchoredPosition = Vector3.zero;
+	}
+	
+	public float Horizontal()
+	{
+		if(inputVector.x != 0)
+			return inputVector.x;
+		else
+			return Input.GetAxis("Horizontal");
+
+	}
+
+	public float Vertical()
+	{
+		if(inputVector.z != 0)
+			return inputVector.z;
+		else
+			return Input.GetAxis("Vertical");
+		
+	}
+
+}
